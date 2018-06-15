117c094e73c4458fdbbc791c58ea36400303b7a0
Jay Pearson <jaypearsonjr@gmail.com>
2018-06-15 00:51:37 -0400

    package updates

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { ItemDetailsPage } from '../item-details/item-details';
-
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
